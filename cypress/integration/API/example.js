51107375704fb8a76c830628e302a50b1a1032db
maryRom01 <tester.automation.11@gmail.com>
2022-12-02 23:15:00 -0600

    add tests with different assertions syntax

--- cypress/integration/API/example.js
+++ cypress/integration/API/example.js
@@ -1,15 +1,48 @@
+import globalValues from '../../support/utils/globalValues'
+
 describe("Testing API Endpoints using Cypress", () => {
 
     const BASE_URL = Cypress.env('mainUrl')
     it("Test GET Request", () => {
         cy.request({
-            method: 'GET',
+            method: globalValues.method.GET,
             url: BASE_URL
         }).then((response) => {
-            console.log(response)
-            expect(response.status).to.eq(200)
-            expect(response.statusText).to.eq("OK")
+            expect(response.status).to.eq(globalValues.statusCode[200])
+            expect(response.statusText).to.eq(globalValues.statusText.OK)
             expect(response.isOkStatusCode).to.eq(true)
         })
+    })
+
+    it("Test GET Request /gen/clients - expect", () => {
+        cy.request({
+            method: globalValues.method.GET,
+            url: BASE_URL + `api/gen/clients`
+        }).then(({body}) => {
+            console.log(body)
+            expect(body).to.have.length(56)
+            expect(body[0]).to.be.a('string')
+            expect(body[0]).to.eq('ada')
+            expect(body).contains('java')
+            expect(body).contains('javascript')
+        })
+    })
+
+    it("Test GET Request /gen/clients - should", () => {
+        cy.request({
+            method: globalValues.method.GET,
+            url: BASE_URL + `api/gen/clients`
+        })
+        .its('body')
+        .should('include', 'android')
+    })
+
+    it("Test GET Request /gen/clients - alias", () => {
+        cy.request({
+            method: globalValues.method.GET,
+            url: BASE_URL + `api/gen/clients`
+        }).as('request')
+        cy.get('@request').its('status').should('equal', 200)
+        cy.get('@request').its('headers').its('content-type').should('include', 'application/json')
     })
 })
---
+++ cypress/support/utils/globalValues.js
@@ -0,0 +1,11 @@
+export default {
+    method: {
+        GET: 'GET'
+    },
+    statusCode: {
+        200: 200
+    },
+    statusText: {
+        OK: 'OK'
+    }
+}
